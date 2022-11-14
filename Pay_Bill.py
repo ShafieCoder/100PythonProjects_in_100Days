901f188dfb711bac42947ee57ba56896d1eeb880
Fatemeh Shafie <94718201+ShafieCoder@users.noreply.github.com>
2022-11-14 10:24:26 -0800

    Create who pay bill in file pay_bill.py
    Notice when you enter the names, you should add space after each comma, otherwise, it will consider as one name

---
+++ Pay_Bill.py
@@ -0,0 +1,18 @@
+
+import random
+# set a random seed that make the results reproducible
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+# It seprate names and put them in a list that seperated by a comma and a space
+names = names_string.split(", ")
+
+
+# find the number of people that share the bill and pick the name should pay for others
+number_of_people = len(names)
+random_choice = random.randint(0,number_of_people -1)
+payer = names[random_choice]
+print(payer + " is going to buy the meal today!")
+# Sara, Sina, Soheil, Saba, Sohrab
