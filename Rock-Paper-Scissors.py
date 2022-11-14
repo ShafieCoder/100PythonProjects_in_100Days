5b25a1e2f3233d060884953710c2be47968c92b6
Fatemeh Shafie <94718201+ShafieCoder@users.noreply.github.com>
2022-11-14 15:16:09 -0800

    Update Rock-Paper-Scissors.py

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -30,20 +30,21 @@ scissors = '''
 # scissors (2) win against paper (1)
 # paper(1) win against rock(0)
 import random
-game_images = [rock, paper, scissors]
 player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 computer_choice = random.randint(0,2)
-print(f"User choice:\n {game_images[player_choice]}\n")
-print(f"computer choice:\n {game_images[computer_choice]}\n")
+game_images = [rock, paper, scissors]
+print(game_images[player_choice])
+print(f"Computer choice:\n {game_images[computer_choice]}")
 
-
-if player_choice >= 3 or player_choice < 0:
-    print("you typed the wrong number!")
+if player_choice >= 3 or player_choice <0:
+  print("You typed a wrong number!!")
 elif player_choice == 0 and computer_choice == 2:
-  print("Congratulation! You Won!")
-elif player_choice == 2 and computer_choice == 1:
-  print("Congratulation! You Won!")
-elif player_choice == 1 and computer_choice == 0:
-  print("Congratulation! You won!")
+  print("You Won!")
+elif computer_choice == 0 and player_choice == 2:
+  print("You lost.")
+elif player_choice > computer_choice:
+  print("You won!")
+elif player_choice == computer_choice:
+  print("It is a draw.")
 else:
-  print("Unfortunately, You lost :(")
+  print("You lost.")
